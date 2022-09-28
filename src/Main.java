519fa374f655709f22eff2245a514c0d08251b4c
sergey_iagfarov <dedaz27@mail.ru>
2022-09-28 17:15:57 +0500

    задание 2

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        int age = 20;
-        if (age >= 18) {
-        System.out.println("Поздравляю с совершеннолетием!");
-    }
-        if (age <= 18)
-            System.out.println("Возраст совершеннолетия еще не наступил, нужно подождать");
+        int capacity = 102;
+        int seatsCount = 60;
+        int stayCount = capacity - seatsCount; // стоячие места
+        int seatsUsed = 59;
+        int stayUsed = 42;
+        if(seatsUsed < seatsCount);
+        System.out.println("Есть еще " + (seatsCount - seatsUsed) + " сидячих мест ");
+        if(stayUsed < stayCount);
+        System.out.println("Есть еще " + (stayCount - stayUsed) + " стоячих мест ");
+
+
         }
 }
