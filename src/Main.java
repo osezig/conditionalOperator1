831fc0b4c10b28bc2a71cd920c77792a6433ccc2
sergey_iagfarov <dedaz27@mail.ru>
2022-09-28 16:03:17 +0500

    задание 1

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int age = 20;
+        if (age >= 18) {
+        System.out.println("Поздравляю с совершеннолетием!");
+    }
+        if (age <= 18)
+            System.out.println("Возраст совершеннолетия еще не наступил, нужно подождать");
+        }
+}
